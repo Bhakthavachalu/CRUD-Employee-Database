cb2f9c945f6f15244eb4f0dcfd1886f550ace167
Bhakthavachalu <bhakthavachalu6@gmail.com>
2024-02-02 14:05:36 +0530

    First commit

---
+++ Springboot-thymeleaf-crud-employee-database/src/main/java/com/example/Springbootthymeleafcrudemployeedatabase/SpringbootThymeleafCrudEmployeeDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.Springbootthymeleafcrudemployeedatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootThymeleafCrudEmployeeDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootThymeleafCrudEmployeeDatabaseApplication.class, args);
+	}
+
+}
---
+++ Springboot-thymeleaf-crud-employee-database/src/test/java/com/example/Springbootthymeleafcrudemployeedatabase/SpringbootThymeleafCrudEmployeeDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Springbootthymeleafcrudemployeedatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootThymeleafCrudEmployeeDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
